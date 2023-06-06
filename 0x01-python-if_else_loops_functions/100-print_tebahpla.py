e01ca8ddd7a4d29dd5b6bba49b1881c30218abc5
abdelmalek <abdelmalek@gmail.com>
2023-06-06 08:52:45 -0700

    task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for i in range(90, 64, -1):
-    print('{}{}'.format(chr(i + 32), chr(i)), end='')
+for i in range(122, 96, -1):
+    print("{}".format(chr(i) if i % 2 == 0 else chr(i).upper()), end="")

97a9e8e9f2f036f621170337dac02a7612ed9330
abdelmalek <abdelmalek@gmail.com>
2023-06-08 01:39:27 -0700

    task 5

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+This program imports the variable `a` from the file `variable_load_5.py`
+and prints its value.
+"""
+
+if __name__ == "__main__":
+    import variable_load_5
+
+    print(variable_load_5.a)

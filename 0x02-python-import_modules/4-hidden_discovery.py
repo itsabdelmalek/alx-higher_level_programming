7a8a2b2602348c39108cd438e95bbb840e7f8efe
abdelmalek <abdelmalek@gmail.com>
2023-06-08 02:24:47 -0700

    fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python3
-
-"""
-This program prints all the names defined in the compiled module hidden_4.pyc.
-"""
-
-import dis
-import types
-
+import hidden_4
 if __name__ == "__main__":
-    with open("hidden_4.pyc", "rb") as file:
-        code_object = types.CodeType.from_traceback(file.read()[16:], file.read())
-        names = set()
-
-        for instruction in dis.get_instructions(code_object):
-            if instruction.opname == "LOAD_GLOBAL":
-                name = instruction.argval
-                if not name.startswith("__"):
-                    names.add(name)
-
-    for name in sorted(names):
-        print(name)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)

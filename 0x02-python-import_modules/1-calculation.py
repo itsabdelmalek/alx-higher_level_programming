ddf93b9dd38b1b239385ef172a5042dbde4a3c86
abdelmalek <abdelmalek@gmail.com>
2023-06-08 02:18:10 -0700

    fix

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,15 +2,8 @@
 if __name__ == "__main__":
     a = 10
     b = 5
-
-    import calculator_1
-
-    result_sum = calculator_1.add(a, b)
-    result_diff = calculator_1.subtract(a, b)
-    result_mul = calculator_1.multiply(a, b)
-    result_div = calculator_1.divide(a, b)
-
-    print("The sum of {} and {} is: {}".format(a, b, result_sum))
-    print("The difference between {} and {} is: {}".format(a, b, result_diff))
-    print("The product of {} and {} is: {}".format(a, b, result_mul))
-    print("The division of {} by {} is: {}".format(a, b, result_div))
+    from calculator_1 import add, sub, mul, div
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))

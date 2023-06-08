ceb4490fba63574f03f8f09c94cae97b5fe163b7
abdelmalek <abdelmalek@gmail.com>
2023-06-08 01:30:17 -0700

    task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+This program imports the function `add` from the file `add_0.py`
+and prints the result of the addition 1 + 2 = 3.
+"""
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+
+    from add_0 import add
+
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))

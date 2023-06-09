44d720d7f9997cc9e748525a94bceb59cd92226b
abdelmalek <abdelmalek@gmail.com>
2023-06-09 00:43:00 -0700

    task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
+
+if __name__ == "__main__":
+    # Example usage
+    lst = [1, 2, 3, 4, 5]
+    index = 2
+    result = element_at(lst, index)
+    print(result)

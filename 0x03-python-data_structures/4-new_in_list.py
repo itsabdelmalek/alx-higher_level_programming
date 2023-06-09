f7652b7bb2d3d8db77d21507e1d5e38a422cdcae
abdelmalek <abdelmalek@gmail.com>
2023-06-09 00:51:48 -0700

    task 5

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list[:]
+    new_list = my_list[:]
+    new_list[idx] = element
+    return new_list
+
+if __name__ == "__main__":
+    # Example usage
+    lst = [1, 2, 3, 4, 5]
+    index = 2
+    new_element = 10
+    result = new_in_list(lst, index, new_element)
+    print(result)
---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
+
+if __name__ == "__main__":
+    # Example usage
+    string = "Hello, World! This is a test string."
+    result = no_c(string)
+    print(result)

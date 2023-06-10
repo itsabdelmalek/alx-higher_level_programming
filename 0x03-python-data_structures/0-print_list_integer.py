d42f186384e6eb23b2bf7871a3f75cd4d818f71d
abdelmalek <abdelmalek@gmail.com>
2023-06-10 01:05:08 -0700

    fix

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+
 def print_list_integer(my_list=[]):
     for num in my_list:
         print("{:d}".format(num))
-        if __name__ == "__main__":
+
+
+if __name__ == "__main__":
     print_list_integer()

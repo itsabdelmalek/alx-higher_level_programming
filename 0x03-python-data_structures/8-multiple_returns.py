8efa8a2daa20497d9a33d60810c0e71e45fffd60
abdelmalek <abdelmalek@gmail.com>
2023-06-10 01:40:12 -0700

    fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -6,8 +6,8 @@ def multiple_returns(sentence):
     else:
         return (len(sentence), sentence[0])
 
+
 if __name__ == "__main__":
-    # Example usage
     text = "Hello, World!"
     result = multiple_returns(text)
     print(result)

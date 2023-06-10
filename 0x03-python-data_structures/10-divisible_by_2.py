ba65e8987b069a67de9acf64bcfea1b01fc51b06
abdelmalek <abdelmalek@gmail.com>
2023-06-10 01:41:41 -0700

    fix

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,8 +3,8 @@
 def divisible_by_2(my_list=[]):
     return [num % 2 == 0 for num in my_list]
 
+
 if __name__ == "__main__":
-    # Example usage
     lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     result = divisible_by_2(lst)
     print(result)

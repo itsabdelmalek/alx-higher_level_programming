1358563ae57e17996e3534a1e0204acb7dbf7edb
abdelmalek <abdelmalek@gmail.com>
2023-06-14 00:57:11 -0700

    task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    Replace or add a key/value pair in a dictionary.
+    """
+    # Update the value of the key if it exists, or add a new key/value pair
+    a_dictionary[key] = value

7346db1b4d25e6889ca0377fa2c69707c3af076b
abdelmalek <abdelmalek@gmail.com>
2023-06-26 06:03:51 -0700

    fix

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def raise_exception():
-    # pycodestyle will check for code style conformity
 
     raise TypeError("Type exception raised")
--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def raise_exception_msg(message=""):
-    # pycodestyle will check for code style conformity
 
     raise NameError(message)

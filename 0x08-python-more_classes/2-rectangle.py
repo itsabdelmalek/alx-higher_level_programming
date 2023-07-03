b256b99b8d6de33416f4751cdb9c22ef6fc9ed46
abdelmalek <abdelmalek@gmail.com>
2023-07-03 02:30:43 -0700

    fix

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -60,4 +60,6 @@ class Rectangle:
         """
         Calculates and returns the perimeter of the rectangle
         """
+        if self.__width == 0 or self.__height == 0:
+            return (0)
         return 2 * (self.__width + self.__height)

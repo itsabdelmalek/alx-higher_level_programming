cbfc473806911a1bc0d07d9fb32aeb337161e89e
abdelmalek <abdelmalek@gmail.com>
2023-08-16 03:04:02 -0700

    fix

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- List privileges of MySQL users user_0d_1 and user_0d_2
+-- Lists all privileges of MySQL users 'user_0d_1'
+-- and 'user_0d_2' in 'localhost'
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';

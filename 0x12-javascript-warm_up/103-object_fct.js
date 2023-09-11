5812e04563a137ab62b875ed62dfdfc581bfb6b9
abdelmalek <abdelmalek@gmail.com>
2023-09-11 03:12:47 -0700

    fix

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,17 +1,19 @@
 #!/usr/bin/node
-let myObject = {
+const myObject = {
   type: 'object',
-  value: 12
+  value: 12,
+  incr: function() {
+    this.value++;
+  }
 };
-console.log(myObject);
 
-myObject.incr = function () {
-  this.value++;
-};
+console.log(myObject);
 
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
